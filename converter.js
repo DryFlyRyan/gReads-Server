26221c2755f1f29ee33777f22afca2aed6300386
Ryan Douglas <ryan@luckydogdigital.com>
2016-01-17 16:24:06 -0700

    weird. I wrote 300 lines and it didn't work. That's never happened before.

--- converter.js
+++ converter.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
+var crud = require('./crud')
 
 //Converter Class
 var Converter = require("csvtojson").Converter;
@@ -6,8 +7,93 @@ var converter = new Converter({});
 
 //end_parsed will be emitted once parsing finished
 converter.on("end_parsed", function (jsonArray) {
-   console.log(jsonArray); //here is your result jsonarray
+   saveArray(jsonArray);
 });
 
 //read from file
 require("fs").createReadStream("./csv_imports/gReads_import.csv").pipe(converter);
+
+function saveArray(array) {
+  for (var i = 0; i < array.length; i++) {
+    var book = array[i];
+    searchThenAddBook(book)
+    .then(function(id) {
+      var bookID = id;
+      Promise.all([
+        searchThenAddAuthor(
+          book['Author 1 First Name'],
+          book['Author 1 Last Name'],
+          book['Author 1 Biography'],
+          book['Author 1 Portrait URL'],
+          bookID
+        ),
+        searchThenAddAuthor(
+          book['Author 2 First Name'],
+          book['Author 2 Last Name'],
+          book['Author 2 Biography'],
+          book['Author 2 Portrait URL'],
+          bookID
+        ),
+        searchThenAddAuthor(
+          book['Author 3 First Name'],
+          book['Author 3 Last Name'],
+          book['Author 3 Biography'],
+          book['Author 3 Portrait URL'],
+          bookID
+        ).then(function(){
+          resolve('Book Added')
+        })
+      ])
+    })
+  }
+}
+
+function searchThenAddBook(book){
+  return new Promise(function(resolve, reject) {
+    crud.Book.searchBook(book['Book Title'])
+    .then(function(book) {
+      console.log(book);
+      if(book.length < 1) {
+        crud.Book.createBook(
+          book['Book Title'],
+          book['Book Genre'],
+          book['Book Description'],
+          book['Book Cover URL']
+        ).then(function(id) {
+          resolve(id);
+        })
+      } else {
+        throw new Error('Book already exists')
+      }
+    })
+  })
+}
+
+function searchThenAddAuthor(first_name, last_name, biography, photo_url, book_id){
+  return new Promise(function(resolve, reject) {
+    if(first_name.length > 1) {
+      crud.Author.searchAuthor(first_name, last_name, biography)
+      .then(function(author) {
+        console.log(author);
+        if(author) {
+          crud.Author.createAuthor(
+            first_name,
+            last_name,
+            biography,
+            photo_url
+          )
+          .then(function(id){
+            crud.Author_Book.AuthorBookJoin(book_id, id)
+          })
+          .then(function(id){
+            resolve(id);
+          })
+        } else {
+          throw new Error('Author already exists')
+        }
+      })
+    } else {
+      reject('No Data Listed');
+    }
+  })
+}
--- crud.js
+++ crud.js
@@ -2,7 +2,7 @@ var knex = require('./db/knex');
 var pg = require('pg');
 var config = {
   client: 'pg',
-  connection: process.env.DATABASE_URL || 'postgres://localhost/dander',
+  connection: 'postgres://localhost/dander',
   ssl: true
 }
 require('dotenv').load();
@@ -27,10 +27,10 @@ var Author_Book = function() {
 //User Table CRUD
 function createUser(first_name, last_name, email, password) {
   return User().insert({
-    first_name: first_name,
-    last_name : last_name,
-    email     : email,
-    password  : password
+    first_name  : first_name,
+    last_name   : last_name,
+    email       : email,
+    password    : password
   }, 'id')
 }
 
@@ -42,19 +42,91 @@ function readUsers() {
 
 function readUser(id) {
   return User().select().where({
-    id        : id
+    id          : id
   }).then(function(user) {
     return user;
   })
 }
 
-//Book Table CRUD
-
-
+function searchUser(email) {
+  return User().select().where({
+    email       : email
+  }).first().then(function(user) {
+    return user;
+  })
+}
 
 //Author Table CRUD
 
+function createAuthor(first_name, last_name, biography, photo_url) {
+  return Author().insert({
+    first_name  : first_name,
+    last_name   : last_name,
+    biography   : biography,
+    photo_url   : photo_url
+  }).returning('id').then(function(id){
+    return id;
+  })
+}
 
+function readAuthors() {
+  return Author().select().then(function(authors) {
+    return authors;
+  })
+}
+
+function readAuthor(id) {
+  return Author().select().where({
+    id          : id
+  }).then(function(author) {
+    return author;
+  })
+}
+
+function searchAuthor(first_name, last_name, biography) {
+  return Author().select().where({
+    first_name  : first_name,
+    last_name   : last_name,
+    biography   : biography
+  }).first().then(function(author) {
+    return author;
+  })
+}
+
+//Book Table CRUD
+
+function createBook(title, genre, description, photo_url) {
+  return Book().insert({
+    title       : title,
+    genre       : genre,
+    description : description,
+    photo_url   : photo_url
+  }, 'id').returning('id').then(function(id) {
+    return id;
+  })
+}
+
+function readBooks() {
+  return Book().select().then(function(book){
+    return book;
+  })
+}
+
+function readBook(id) {
+  return Book().select().where({
+    id          : id
+  }).then(function(book){
+    return book;
+  })
+}
+
+function searchBook(title) {
+  return Book().select().where({
+    title       : title
+  }).first().then(function(book) {
+    return book;
+  })
+}
 
 //User_Book Join Table CRUD
 
@@ -62,11 +134,36 @@ function readUser(id) {
 
 //Author_Book Join Table CRUD
 
+function createAuthorBookJoin(book_id, author_id) {
+  return Author_Book().insert({
+    book_id     : book_id,
+    author_id   : author_id
+  }).returning('id').then(function(id){
+    return id;
+  })
+}
+
 module.exports  = {
   //User Functions
-  User          : {
-    createUser  : createUser,
-    readUsers   : readUsers,
-    readUser    : readUser
+  User            : {
+    createUser    : createUser,
+    readUsers     : readUsers,
+    readUser      : readUser,
+    searchUser    : searchUser
+  },
+  Author          : {
+    createAuthor  : createAuthor,
+    readAuthors   : readAuthors,
+    readAuthor    : readAuthor,
+    searchAuthor  : searchAuthor
+  },
+  Book            : {
+    createBook    : createBook,
+    readBooks     : readBooks,
+    readBook      : readBook,
+    searchBook    : searchBook
+  },
+  Author_Book     : {
+    AuthorBookJoin: createAuthorBookJoin
   }
 }
