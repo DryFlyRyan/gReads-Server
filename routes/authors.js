3579e8d0f0e470f430b08f7fb1739dc54b2c8ea3
Ryan Douglas <ryan@luckydogdigital.com>
2016-01-19 09:18:39 -0700

    routes working

--- routes/authors.js
+++ routes/authors.js
@@ -4,22 +4,22 @@ var crud = require('../crud')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  crud.Book.readBooks()
-  .then(function(books) {
-    if (books) {
-      res.json(books);
+  crud.Author.readAuthors()
+  .then(function(authors) {
+    if (authors) {
+      res.json(authors);
     } else {
       res.send('Data not found')
     }
   });
 });
 
-router.post('/readBook', function(req,res,next){
+router.post('/readAuthor', function(req,res,next){
   id = parseInt(req.body.id)
-  crud.Book.readBook(id)
-  .then(function(book){
-    if(book) {
-      res.json(book);
+  crud.Author.readAuthor(id)
+  .then(function(author){
+    if(author) {
+      res.json(author);
     } else {
       res.send('Data not found')
     }
