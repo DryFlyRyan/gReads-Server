90ab66e64d606aac04047272af41e18f991367b1
Ryan Douglas <ryan@luckydogdigital.com>
2016-01-18 20:37:34 -0700

    changing enviro on pg heroku

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,21 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/gReads'
+    connection: {
+      user: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      database: process.env.PG_DATABASE,
+      ssl: true
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
 
   // staging: {
